4df5172bb51023efe5e18225432acb4e9b60d1e3
Raquel Cedazo León <36467661+raquelcedazo@users.noreply.github.com>
2021-04-16 09:02:25 +0200

    First version
    Menu created

---
+++ programa.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define N 100
+
+struct punto{
+	int x;
+	int y;
+};
+
+int main() {
+	
+	FILE *fichero;
+	int puntos, i;
+	struct punto vector[N];
+	
+	fichero = fopen("puntos.txt", "r");
+	if (fichero == NULL) {
+		printf("Error, el fichero no existe\n");
+		return 0;
+	}
+	
+	fscanf(fichero, "%d", &puntos);
+	i=0;
+	// EOF End Of File
+	while (i<12) {
+		fscanf(fichero, "%d %d", &vector[i].x, &vector[i].y);
+		printf("(%d, %d)\n", vector[i].x, vector[i].y);
+		i++;
+	}
+	
+	fclose(fichero);
+	
+	printf("Total de puntos del fichero: %d\n", puntos);
+	
+	// 
+	
+	return 0;
+}
