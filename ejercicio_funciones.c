9238c3e7d7f3d7952a19c669ca5b7d221ac5c067
Raquel Cedazo León <36467661+raquelcedazo@users.noreply.github.com>
2021-05-19 13:51:17 +0200

    Resuelto ejercicio_funciones

--- ejercicio_funciones.c
+++ ejercicio_funciones.c
@@ -10,21 +10,33 @@ int existeUsuario(struct TUsuario lista[], int dimension, struct TUsuario buscad
 
 int main() {
 	struct TUsuario usuarios[N] = {{"jjperez", "1234"}, {"mmmartinez", "1234"}, {"lllopez", "28021993"}};
-	int contador = 3;
+	int contador = 3, resultado;
 	
-	struct Usuario login;
+	struct TUsuario login;
 	
 	printf("Introduce tu usuario:\n");
-	scanf("%s", login.ussername);
+	scanf("%s", login.username);
 	printf("Introduce tu contraseña:\n");
 	scanf("%s", login.password);
 	
 	// Invocar a la funcion
+	resultado = existeUsuario(usuarios, contador, login);
+	
+	if (resultado == 1) {
+		printf("Genial, bienvenido a la aplicacion\n");
+	} else {
+		printf("No tienes permisos para entrar a la aplicacion\n");
+	}
 	
 	return 0;
 }
 
 int existeUsuario(struct TUsuario lista[], int dimension, struct TUsuario buscado) {
-	
-	
+	int i;
+	for (i=0; i<dimension; i++) {
+		if ((strcmp(lista[i].username, buscado.username) == 0) && (strcmp(lista[i].password, buscado.password)==0)) {
+			return 1; // devuelve 1, verdadero 
+		}
+	}
+	return 0; // devuelve falso
 }
